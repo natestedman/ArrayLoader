1e86f838f6e232a42036971068132a7248042ee9
Nate Stedman <natesm@gmail.com>
2015-11-16 18:57:08 -0500

    Add tests for LoadRequest.

---
+++ ArrayLoaderTests/LoadRequestTests.swift
@@ -0,0 +1,47 @@
+// ArrayLoader
+// Written in 2015 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+@testable import ArrayLoader
+import XCTest
+
+class LoadRequestTests: XCTestCase
+{
+    func testNext()
+    {
+        let request = LoadRequest<Int, Int>.Next(current: [], info: 0)
+        XCTAssertTrue(request.isNext)
+        XCTAssertFalse(request.isPrevious)
+    }
+    
+    func testPrevious()
+    {
+        let request = LoadRequest<Int, Int>.Previous(current: [], info: 0)
+        XCTAssertTrue(request.isPrevious)
+        XCTAssertFalse(request.isNext)
+    }
+    
+    func testCurrent()
+    {
+        let previous = LoadRequest<Int, Int>.Previous(current: [0, 1, 2, 3], info: 0)
+        XCTAssertEqual(previous.current, [0, 1, 2, 3])
+        
+        let next = LoadRequest<Int, Int>.Previous(current: [0, 1, 2, 3], info: 0)
+        XCTAssertEqual(next.current, [0, 1, 2, 3])
+    }
+    
+    func testInfo()
+    {
+        let previous = LoadRequest<Int, Int>.Next(current: [], info: 10)
+        XCTAssertEqual(previous.info, 10)
+        
+        let next = LoadRequest<Int, Int>.Next(current: [], info: 10)
+        XCTAssertEqual(next.info, 10)
+    }
+}
